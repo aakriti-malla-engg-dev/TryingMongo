2497bfa2cb9337864b735ed5876971de148d1953
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-01-13 14:59:51 +0530

    connection

---
+++ src/main/java/org/example/MongoDB.java
@@ -0,0 +1,36 @@
+package org.example;
+import static com.mongodb.client.model.Filters.eq;
+import static javax.management.Query.lt;
+
+import com.mongodb.client.*;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
+
+import org.bson.conversions.Bson;
+
+public class MongoDB {
+    public static void main( String[] args ) {
+
+        String uri = "mongodb://127.0.0.1:27017";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("company");
+            MongoCollection<Document> collection = database.getCollection("employee");
+
+            MongoCursor<Document> cursor = collection.find().iterator();
+            while (cursor.hasNext()) {
+                System.out.println("collection is " +cursor.next() );
+            }
+
+//            Document doc = collection.find().first();
+//            if (doc != null) {
+//                System.out.println(doc.toJson());
+//            } else {
+//                System.out.println("No matching documents found.");
+//            }
+        }
+    }
+}
+
+
